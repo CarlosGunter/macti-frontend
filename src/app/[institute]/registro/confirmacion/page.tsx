67b3420412c0403ed201af34d3e6a660fd5b1903
CarlosGunter <carlosgt1702@gmail.com>
2025-09-23 18:36:39 -0600

    feat: se mejoró el diseño de la página de confirmación con estilos de cuadrícula

--- src/app/[institute]/registro/confirmacion/page.tsx
+++ src/app/[institute]/registro/confirmacion/page.tsx
@@ -2,9 +2,11 @@ import CreateAccount from "@/domains/auth/components/CreateAccout";
 
 export default function ConfirmacionPage() {
   return (
-    <div>
-      <h1>Solicitud de cuenta aprobada</h1>
-      <p>Escribe una contraseña para completar tu registro:</p>
+    <div className="grid gap-8 p-4 justify-center">
+      <div>
+        <h1>Solicitud de cuenta aprobada</h1>
+        <p className="text-xs">Escribe una contraseña para completar tu registro:</p>
+      </div>
 
       <CreateAccount />
     </div>
