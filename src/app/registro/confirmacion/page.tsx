7b465a49bcdb1b7b1c19ea643b2fcfef6cfb7bb1
CarlosGunter <carlosgt1702@gmail.com>
2025-09-26 00:03:42 -0600

    fix: se corrigió la verificación del token y se actualizó el manejo de la respuesta en el servicio de confirmación

--- src/app/registro/confirmacion/page.tsx
+++ src/app/registro/confirmacion/page.tsx
@@ -1,6 +1,7 @@
 import CreateAccount from "@/domains/auth/components/CreateAccoutForm";
 import { verifyToken } from "@/domains/auth/services/verifyToken";
 import { notFound } from "next/navigation";
+import { use } from "react";
 
 interface ConfirmacionPageProps {
   searchParams: {
@@ -14,12 +15,12 @@ export default async function ConfirmacionPage({ searchParams }: ConfirmacionPag
 
   const userData = await verifyToken(token);
   if (!userData.success) notFound();
-
+  console.log(userData)
   return (
     <div className="grid gap-8 p-4 justify-center">
       <div className="text-center">
         <h1>Solicitud de cuenta aprobada</h1>
-        <p>{userData.name}</p>
+        <p>{userData.data.correo}</p>
         <p className="text-xs">Escribe una contraseña para completar tu registro:</p>
       </div>
 
--- src/domains/auth/services/verifyToken.ts
+++ src/domains/auth/services/verifyToken.ts
@@ -1,24 +1,31 @@
 import { tryCatch } from "@/shared/utils/try-catch";
 
 export async function verifyToken(token: string): Promise<Record<string, any>> {
-  const apiURLBase = process.env.API_URL_BASE || "http://localhost:3000";
-
-  const verifyTokenPromise = fetch(`${apiURLBase}/auth/validate-token?token=${token}`, {
+  const apiURLBase = process.env.API_URL_BASE || "http://localhost:8000";
+  console.log({token})
+  const verifyTokenPromise = fetch(`${apiURLBase}/auth/confirmacion?token=${token}`, {
     method: "GET",
     cache: "no-store",
   });
 
   const verifyTokenResponse = await tryCatch(verifyTokenPromise);
-  if (!verifyTokenResponse.success) return {
-    success: false,
-    message: "Error al verificar el token. Inténtalo de nuevo más tarde."
-  };
-
-  const userData = await tryCatch(verifyTokenResponse.data.json());
-  if (!userData.success) return {
-    success: false,
-    message: "Token inválido."
-  };
+  if (!verifyTokenResponse.success || !verifyTokenResponse.data.ok) {
+    return {
+      success: false,
+      message: "Error al verificar el token. Inténtalo de nuevo más tarde."
+    };
+  }
   
-  return userData.data;
+  const userData = await tryCatch(verifyTokenResponse.data.json());
+  if (!userData.success) {
+    return {
+      success: false,
+      message: "Token inválido."
+    };
+  }
+    
+  return {
+    success: true,
+    data: userData.data.data
+  };
 }
