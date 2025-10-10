01e7b78c9dc716c54e3d2ca3d47dd158bdf6e268
CarlosGunter <carlosgt1702@gmail.com>
2025-10-09 20:47:42 -0600

    feat: se combió la estructura de la página de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import { institutes } from "@/shared/config/institutes";
 export default function Home() {
   return (
     <>
-      <Hero />
-      <div className="max-w-7xl mx-auto p-2">
+      <div className="max-w-7xl mx-auto p-2 my-6 w-full">
         <main className="grid gap-4 w-full">
-          <h2 className="text-center text-2xl font-bold">Institutos</h2>
-          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <h2 className="text-4xl font-bold">Institutos</h2>
+          <section className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3 lg:[&>*:nth-child(4)]:col-span-2 lg:[&>*:last-child]:col-span-2">
             {Object.entries(institutes).map(([key, institute]) => (
               <InstituteCard key={key} id={key} {...institute} />
             ))}
           </section>
         </main>
       </div>
+      <Hero />
     </>
   );
 }
