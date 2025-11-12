f922bb95f3bdd2cb75b81de5158be94e22720ea3
CarlosGunter <carlosgt1702@gmail.com>
2025-11-11 20:22:20 -0600

    feat: se añadieron iconos de visibilidad para las contraseñas en el formulario de creación de cuenta.

--- src/domains/auth/components/CreateAccoutForm.tsx
+++ src/domains/auth/components/CreateAccoutForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useActionState, useState } from "react";
+import VisibilityIcon from "@/assets/svg/visibility";
+import VisibilityOffIcon from "@/assets/svg/visibilityOff";
 import Banner from "@/shared/components/feedback/Banner";
 import Button from "@/shared/components/ui/Button";
 import { createAccountAction } from "../actions/createAccountAction";
@@ -11,6 +13,8 @@ export default function CreateAccount({ userData }: { userData: CreateAccountRes
 
   const [password, setPassword] = useState(state?.data?.new_password || "");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isVisiblePass, setIsVisiblePass] = useState(false);
+  const [isVisibleConfirmPass, setIsVisibleConfirmPass] = useState(false);
 
   const passwordsMatch = password === confirmPassword;
 
@@ -24,26 +28,44 @@ export default function CreateAccount({ userData }: { userData: CreateAccountRes
 
         <label htmlFor="new_password" className="grid gap-1.5 w-full">
           <span>Ingresa una nueva contraseña*</span>
-          <input
-            name="new_password"
-            type="password"
-            placeholder="Nueva contraseña"
-            className={`${!passwordsMatch && confirmPassword ? "border-red-500" : ""}`}
-            defaultValue={state?.data?.new_password || ""}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="grid place-items-center">
+            <input
+              name="new_password"
+              type={isVisiblePass ? "text" : "password"}
+              placeholder="Nueva contraseña"
+              className={`col-start-1 col-end-1 row-start-1 -row-end-1 w-full ${!passwordsMatch && confirmPassword ? "border-red-500" : "border-none"}`}
+              defaultValue={state?.data?.new_password || ""}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="col-start-1 -col-end-1 row-start-1 -row-end-1 place-self-end self-center px-2 py-1.5"
+              onClick={() => setIsVisiblePass(!isVisiblePass)}
+            >
+              {isVisiblePass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            </button>
+          </div>
           <span className="text-xs">Ingresa una contraseña segura.</span>
         </label>
 
         <label htmlFor="confirm_password" className="grid gap-1.5 w-full">
           <span>Confirma tu nueva contraseña*</span>
-          <input
-            name="confirm_password"
-            type="password"
-            placeholder="Confirma tu nueva contraseña"
-            className={`${!passwordsMatch && confirmPassword ? "border-red-500" : ""}`}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
+          <div className="grid place-items-center">
+            <input
+              name="confirm_password"
+              type={isVisibleConfirmPass ? "text" : "password"}
+              placeholder="Confirma tu nueva contraseña"
+              className={`col-start-1 col-end-1 row-start-1 -row-end-1 w-full ${!passwordsMatch && confirmPassword ? "border-red-500" : "border-none"}`}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="col-start-1 -col-end-1 row-start-1 -row-end-1 place-self-end self-center px-2 py-1.5"
+              onClick={() => setIsVisibleConfirmPass(!isVisibleConfirmPass)}
+            >
+              {isVisibleConfirmPass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            </button>
+          </div>
 
           {!passwordsMatch && confirmPassword && (
             <span className="text-xs text-red-500">Las contraseñas no coinciden.</span>
