cb98aa0cdf40be2e6ac10a75879764e72475c49e
CarlosGunter <carlosgt1702@gmail.com>
2026-03-06 21:27:32 -0600

    fix: los botones para login y registro ahora aparecen por defecto en dispositivos pequeños en el componente InstituteCard.

--- src/domains/home/components/InstituteCard.tsx
+++ src/domains/home/components/InstituteCard.tsx
@@ -21,10 +21,10 @@ export default function InstituteCard({ id, name, details, image }: Institute) {
         style={{ "--img": `url(${image.src})` } as React.CSSProperties}
       />
       <div className="grid place-items-center h-48 pointer-events-none grid-rows-1 grid-cols-1 row-start-1 row-end-2 col-start-1 col-end-2">
-        <h2 className="text-center font-bold text-lg transition-[translate] group-hover:-translate-y-11">
+        <h2 className="text-center font-bold text-lg transition-[translate] -translate-y-10 md:translate-0 md:group-hover:-translate-y-10">
           {name}
         </h2>
-        <div className="absolute flex gap-2 bottom-7 opacity-0 translate-y-7 transition-[opacity,translate] group-hover:opacity-100 group-hover:translate-y-0">
+        <div className="absolute flex gap-2 bottom-7 md:opacity-0 md:translate-y-7 transition-[opacity,translate] md:group-hover:opacity-100 md:group-hover:translate-y-0">
           <LoginProvider institute={id}>
             <LoginCardButton />
           </LoginProvider>
