66d04eb468d6d146e87090b79497b68b946f4bcd
CarlosGunter <carlosgt1702@gmail.com>
2026-03-08 21:07:47 -0600

    fix: se actualizó la definición de tipos para los parámetros en varias páginas
    - Se cambió la definición de `params` a `Promise<{ institute: string }>` en el componente Layout.
    - Se modificó la definición de `params` a `Promise<{ course_id: string; institute: string }>` en el componente SolicitudesPage.
    - Se ajustó la definición de `params` a `Promise<{ institute: string }>` en el componente InstitutePage.
    - Se actualizó la definición de `params` a `Promise<{ institute: string }>` en el componente PerfilPage.

--- src/app/[institute]/perfil/page.tsx
+++ src/app/[institute]/perfil/page.tsx
@@ -2,9 +2,9 @@ import { ProfileCard } from "@/domains/users/components/ui/ProfileCard";
 import ProfileTabs from "@/shared/components/common/ProfileTabs";
 
 interface PerfilPageProps {
-  params: {
+  params: Promise<{
     institute: string;
-  };
+  }>;
 }
 
 export default async function PerfilPage({ params }: PerfilPageProps) {
