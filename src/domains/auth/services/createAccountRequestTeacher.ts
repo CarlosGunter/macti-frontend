3489072718b0107e73353e7a3d7f3ca693497bdd
CarlosGunter <carlosgt1702@gmail.com>
2026-04-12 04:30:33 -0600

    refactor: se reemplazó la importación de apiURLBase por la URL de la API externa en varios servicios de autenticación
    - Actualizada la función CreateAccount para utilizar la URL de la API externa.
    - Modificada la función createAccountRequest para obtener la URL de la API externa.
    - Ajustada la función createAccountRequestTeacher para usar la URL de la API externa.
    - Cambiada la función fetchAccountInfo para que utilice la URL de la API externa.

--- src/domains/auth/services/createAccountRequestTeacher.ts
+++ src/domains/auth/services/createAccountRequestTeacher.ts
@@ -1,16 +1,18 @@
-import { apiURLBase } from "@/shared/config/api";
 import { processFetch } from "@/shared/utils/process-fetch";
 import type { AccountRequestTeacherPayload } from "../schemas/accountRequestTeacherSchema";
 
 export async function createAccountRequestTeacher(
   userRequestData: AccountRequestTeacherPayload,
 ) {
-  const accountRequestPromise = fetch(`${apiURLBase}/auth/request-account/teacher`, {
-    method: "POST",
-    cache: "no-store",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userRequestData),
-  });
+  const accountRequestPromise = fetch(
+    `${process.env.NEXT_PUBLIC_EXTERNAL_API_URL}/auth/request-account/teacher`,
+    {
+      method: "POST",
+      cache: "no-store",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userRequestData),
+    },
+  );
 
   const [error, accountRequestResult] = await processFetch(accountRequestPromise);
   if (error) return { success: false, error: accountRequestResult };
