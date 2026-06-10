1c516147e19dde0dfe19f9cf89a984ff499b4e64
CarlosGunter <carlosgt1702@gmail.com>
2026-06-10 15:31:35 -0600

    style: se modificó el texto del botón de "Solicitudes de cursos de profesores" a "Solicitudes de cursos"

--- src/shared/components/common/ProfileTabs.tsx
+++ src/shared/components/common/ProfileTabs.tsx
@@ -39,7 +39,7 @@ export default function ProfileTabs({ institute }: ProfileTabsProps) {
               : "text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
           }`}
         >
-          Solicitudes de cursos de profesores
+          Solicitudes de cursos
         </button>
       </div>
 
