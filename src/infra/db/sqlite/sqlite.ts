9c635481b9efb0bff9f1f3cc38c430f5a149cc41
CarlosGunter <carlosgt1702@gmail.com>
2026-06-10 02:30:51 -0600

    refactor: se movió la declaración de dbPath dentro de la función initSQLite para mejorar la encapsulación

--- src/infra/db/sqlite/sqlite.ts
+++ src/infra/db/sqlite/sqlite.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 
-const dbPath = process.env.DATABASE_URL || "@/../data/auth.sqlite";
-
 export const initSQLite = () => {
+  const dbPath = process.env.DATABASE_URL || "@/../data/auth.sqlite";
+
   return new Database(dbPath);
 };
