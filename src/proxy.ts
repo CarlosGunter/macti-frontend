0e1b273f372e440d9dacaa278f643575cd5aacc8
CarlosGunter <carlosgt1702@gmail.com>
2026-06-19 19:49:49 -0600

    refactor: se eliminó la dependencia de la variable de entorno NEXT_PUBLIC_APP_URL en la construcción de URLs
    - Se actualizó la función GET para utilizar directamente url.origin.
    - Se modificó la función buildRedirectURL para que también use request.nextUrl.origin.

--- src/proxy.ts
+++ src/proxy.ts
@@ -25,7 +25,7 @@ function buildRedirectURL(institute: string, request: NextRequest) {
   );
   const redirectUrl = new URL(
     `/api/proxy/${institute}/keycloak/login?callbackURL=${callbackURL}`,
-    process.env.NEXT_PUBLIC_APP_URL || request.nextUrl.origin,
+    request.nextUrl.origin,
   );
 
   return redirectUrl;
